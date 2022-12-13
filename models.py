2e124265773d1d2d9325f26b0f8ca8433475eb16
Kelvin Ivan <108493997+zzKelvin@users.noreply.github.com>
2022-12-13 15:45:11 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from Models_SqlAlchemy import Pessoas
+from Models_SqlAlchemy import Pessoas, Usuarios
 
 def insere_pessoas():
     pessoa = Pessoas(nome='Galleani', idade=25)
@@ -20,6 +20,16 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Felipe').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login,senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 if __name__ == '__main__':
-    insere_pessoas()
-    consulta_pessoas()
+    insere_usuario('rafael', '1234')
+    consulta_todos_usuarios()
+    #insere_pessoas()
+    #consulta_pessoas()
